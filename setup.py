7c00c0063c7c5ad96b1100ed50e4a2b49fe0aee8
Andreas Kloeckner <inform@tiker.net>
2013-06-09 14:05:11 -0400

    Switch to setuptools, update aksetup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: latin1 -*-
 
+
 def main():
-    import distribute_setup
-    distribute_setup.use_setuptools()
+    import ez_setup
+    ez_setup.use_setuptools()
 
     from setuptools import setup
 
@@ -15,7 +16,8 @@ def main():
 
     version_dict = {}
     init_filename = "boxtree/version.py"
-    exec(compile(open(init_filename, "r").read(), init_filename, "exec"), version_dict)
+    exec(compile(open(init_filename, "r").read(), init_filename, "exec"),
+            version_dict)
 
     setup(name="boxtree",
           version=version_dict["VERSION_TEXT"],
@@ -23,28 +25,28 @@ def main():
           long_description=open("README.rst", "rt").read(),
           author="Andreas Kloeckner",
           author_email="inform@tiker.net",
-          license = "MIT",
+          license="MIT",
           url="http://wiki.tiker.net/BoxTree",
           classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Other Audience',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: English',
-            'Programming Language :: Python',
-            # We use conditional expressions, so 2.5 is the bare minimum.
-            'Programming Language :: Python :: 2.5',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            # 3.x has not yet been tested.
-            'Topic :: Scientific/Engineering',
-            'Topic :: Scientific/Engineering :: Information Analysis',
-            'Topic :: Scientific/Engineering :: Mathematics',
-            'Topic :: Scientific/Engineering :: Visualization',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: Utilities',
-            ],
+              'Development Status :: 3 - Alpha',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Other Audience',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: MIT License',
+              'Natural Language :: English',
+              'Programming Language :: Python',
+              # We use conditional expressions, so 2.5 is the bare minimum.
+              'Programming Language :: Python :: 2.5',
+              'Programming Language :: Python :: 2.6',
+              'Programming Language :: Python :: 2.7',
+              # 3.x has not yet been tested.
+              'Topic :: Scientific/Engineering',
+              'Topic :: Scientific/Engineering :: Information Analysis',
+              'Topic :: Scientific/Engineering :: Mathematics',
+              'Topic :: Scientific/Engineering :: Visualization',
+              'Topic :: Software Development :: Libraries',
+              'Topic :: Utilities',
+              ],
 
           packages=["boxtree"],
           install_requires=[
@@ -56,8 +58,6 @@ def main():
 
           # 2to3 invocation
           cmdclass={'build_py': build_py})
-
-
 
 
 if __name__ == '__main__':
