a55da65a2e64da27c5ca32ea40a078b839eb71cc
Hao Gao <gaohao95@gmail.com>
2017-10-22 20:01:51 -0500

    Construct ancestor mask for all boxes in a rank

--- boxtree/dfmm.py
+++ boxtree/dfmm.py
@@ -39,45 +39,64 @@ def drive_dfmm(traversal, expansion_wrangler, src_weights):
 
     # }}}
 
-    # {{{ Distribute problem parameters
+    # {{{ Distribute tree parameters
 
     if current_rank == 0:
         tree = traversal.tree
-        parameters = {"nsources":tree.nsources, 
+        # TODO: distribute more parameters of the tree
+        parameters = {"sources_are_targets": tree.sources_are_targets,
+                      "sources_have_extent": tree.sources_have_extent,
+                      "nsources":tree.nsources,
+                      "nboxes":tree.box_source_starts.shape[0], 
                       "dimensions":tree.sources.shape[0], 
-                      "coord_dtype":tree.coord_dtype}
+                      "coord_dtype":tree.coord_dtype,
+                      "box_id_dtype":tree.box_id_dtype}
     else:
         parameters = None
     parameters = comm.bcast(parameters, root=0)
     
     # }}}
 
-    # {{{ Distribute source particles
-    
-    num_sources_per_rank = (parameters["nsources"] + total_rank - 1) // total_rank
-    sources = []
+    # {{{ Fill tree parameters to the locally essentail tree
 
-    for i in range(parameters["dimensions"]):
-        # Prepare send buffer
-        if current_rank == 0:
-            sendbuf = np.empty((num_sources_per_rank * total_rank,), 
-                               dtype=parameters['coord_dtype'])
-            sendbuf[:parameters["nsources"]] = tree.sources[i]
-        else:
-            sendbuf = None
+    from boxtree import Tree
+    letree = Tree()
+    # TODO: add more parameters to the locally essential tree
+    letree.sources_are_targets = parameters["sources_are_targets"]
 
-        # Prepare receive buffer
-        recvbuf = np.empty((num_sources_per_rank,), dtype=parameters['coord_dtype'])
+    # }}}
 
-        # Scatter send buffer
-        comm.Scatter(sendbuf, recvbuf, root=0)
+    # {{{ Construct locally essential tree mask for each rank
 
-        # Trim the receive buffer for the last rank
-        if current_rank == total_rank - 1:
-            num_sources_current_rank = parameters["nsources"] - \
-                num_sources_per_rank * (total_rank - 1)
-            sources.append(recvbuf[:num_sources_current_rank])
-        else:
-            sources.append(recvbuf)
-    
+    # Problem: Current implementation divides all boxes with targets evenly across all 
+    # ranks. This scheme is subject to significant load imbalance. A better way to do 
+    # this is to assign a weight to each box according to its interaction list, and then 
+    # divides boxes evenly by the total weights.
+
+    if current_rank == 0:
+        # mask[i][j] is true iff box j is in the locally essential tree of rank i
+        mask = np.zeros((total_rank, parameters["nboxes"]), dtype=bool)
+        target_boxes = traversal.target_boxes
+        num_boxes_per_rank = (len(target_boxes) + total_rank - 1) // total_rank
+        
+        for i in range(total_rank):
+            # Get the start and end box index for rank i 
+            box_start_idx = num_boxes_per_rank * i
+            if current_rank == total_rank - 1:
+                box_end_idx = len(target_boxes)
+            else:
+                box_end_idx = num_boxes_per_rank * (i + 1)
+
+            # Mark all ancestors of boxes of rank i
+            new_mask = np.zeros(parameters["nboxes"], dtype=bool)
+            new_mask[target_boxes[box_start_idx:box_end_idx]] = True
+            while np.count_nonzero(new_mask) != 0:
+                np.logical_or(mask[i, :], new_mask, out=mask[i, :])
+                new_mask_idx = np.nonzero(new_mask)
+                new_mask_parent_idx = tree.box_parent_ids[new_mask_idx]
+                new_mask[:] = False
+                new_mask[new_mask_parent_idx] = True
+                new_mask = np.logical_and(new_mask, np.logical_not(mask[i, :]), 
+                                          out=new_mask)
+
     # }}}
