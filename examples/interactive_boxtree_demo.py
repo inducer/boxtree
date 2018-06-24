1585a09bfe423dbc74474b5d970ff46636df2ca1
xywei <wxy0516@gmail.com>
2018-06-24 20:20:48 +0800

    Quad points in the dicionary order for each box

--- examples/interactive_boxtree_demo.py
+++ examples/interactive_boxtree_demo.py
@@ -8,11 +8,14 @@ ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
 
 tree = boxtree.tree_interactive_build.BoxTree()
-tree.generate_uniform_boxtree(queue, nlevels=3)
+tree.generate_uniform_boxtree(queue, nlevels=3, root_extent=2,
+        # root_vertex=(0,0,0))
+        root_vertex=(0,0))
 
 # the default quad formula uses cell centers and cell measures
 from modepy import GaussLegendreQuadrature
-quadrature_formula = GaussLegendreQuadrature(1)
+n_q_points = 5
+quadrature_formula = GaussLegendreQuadrature(n_q_points - 1)
 print(quadrature_formula.nodes, quadrature_formula.weights)
 quad = boxtree.tree_interactive_build.QuadratureOnBoxTree(tree,
         quadrature_formula)
@@ -21,8 +24,8 @@ cell_measures = quad.get_cell_measures(queue)
 q_points = quad.get_q_points(queue)
 q_weights = quad.get_q_weights(queue)
 
-print(q_points)
-print(q_weights)
+# print(q_points)
+# print(q_weights)
 
 # print(q_points - cell_centers)
 # print(q_weights - cell_measures)
@@ -33,6 +36,11 @@ plt = BoxTreePlotter(tree.get(queue))
 plt.draw_tree()
 plt.set_bounding_box()
 plt.draw_box_numbers()
+
+qx = q_points[0].get(queue)
+qy = q_points[1].get(queue)
+
+pt.plot(qx, qy, '*')
 
 pt.tick_params(
     axis='x',          # changes apply to the x-axis
