eda8122c5d7bca68d43c9ad3fd60aee147eced88
Hao Gao <gaohao95@gmail.com>
2018-11-10 22:14:58 -0600

    Add list 1 cost counter

---
+++ boxtree/cost.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pyopencl as cl
+import pyopencl.array  # noqa: F401
+from pyopencl.elementwise import ElementwiseKernel
+from pyopencl.tools import dtype_to_ctype
+from abc import ABC, abstractmethod
+from mako.template import Template
+
+
+class CostCounter(ABC):
+    @abstractmethod
+    def collect_direct_interaction_data(self, traversal, tree):
+        pass
+
+
+class CLCostCounter(CostCounter):
+    def __init__(self, queue):
+        self.queue = queue
+
+    def collect_direct_interaction_data(self, traversal, tree):
+        ntarget_boxes = len(traversal.target_boxes)
+        particle_id_dtype = tree.particle_id_dtype
+        box_id_dtype = tree.box_id_dtype
+
+        count_direct_interaction_knl = ElementwiseKernel(
+            self.queue.context,
+            Template("""
+                ${particle_id_t} *srcs_by_itgt_box,
+                ${box_id_t} *source_boxes_starts,
+                ${box_id_t} *source_boxes_lists,
+                ${particle_id_t} *box_source_counts_nonchild
+            """).render(
+                particle_id_t=dtype_to_ctype(particle_id_dtype),
+                box_id_t=dtype_to_ctype(box_id_dtype)
+            ),
+            Template("""
+                ${particle_id_t} nsources = 0;
+                ${box_id_t} source_boxes_start_idx = source_boxes_starts[i];
+                ${box_id_t} source_boxes_end_idx = source_boxes_starts[i + 1];
+
+                for(${box_id_t} cur_source_boxes_idx = source_boxes_start_idx;
+                    cur_source_boxes_idx < source_boxes_end_idx;
+                    cur_source_boxes_idx++)
+                {
+                    ${box_id_t} cur_source_box = source_boxes_lists[
+                        cur_source_boxes_idx
+                    ];
+                    nsources += box_source_counts_nonchild[cur_source_box];
+                }
+
+                srcs_by_itgt_box[i] = nsources
+            """).render(
+                particle_id_t=dtype_to_ctype(particle_id_dtype),
+                box_id_t=dtype_to_ctype(box_id_dtype)
+            ),
+            name="count_direct_interaction"
+        )
+
+        box_source_counts_nonchild_dev = cl.array.to_device(
+            self.queue, tree.box_source_counts_nonchild
+        )
+
+        # List 1
+        nlist1_srcs_by_itgt_box_dev = cl.array.zeros(
+            self.queue, (ntarget_boxes,), dtype=particle_id_dtype
+        )
+        neighbor_source_boxes_starts_dev = cl.array.to_device(
+            self.queue, traversal.neighbor_source_boxes_starts
+        )
+        neighbor_source_boxes_lists_dev = cl.array.to_device(
+            self.queue, traversal.neighbor_source_boxes_lists
+        )
+        count_direct_interaction_knl(
+            nlist1_srcs_by_itgt_box_dev,
+            neighbor_source_boxes_starts_dev,
+            neighbor_source_boxes_lists_dev,
+            box_source_counts_nonchild_dev
+        )
+
+        result = dict()
+        result["nlist1_srcs_by_itgt_box"] = nlist1_srcs_by_itgt_box_dev.get()
+
+        return result
+
+
+class PythonCostCounter(CostCounter):
+    def collect_direct_interaction_data(self, traversal, tree):
+        ntarget_boxes = len(traversal.target_boxes)
+
+        # target box index -> nsources
+        nlist1_srcs_by_itgt_box = np.zeros(ntarget_boxes, dtype=np.intp)
+        nlist3close_srcs_by_itgt_box = np.zeros(ntarget_boxes, dtype=np.intp)
+        nlist4close_srcs_by_itgt_box = np.zeros(ntarget_boxes, dtype=np.intp)
+
+        for itgt_box in range(ntarget_boxes):
+            nlist1_srcs = 0
+            start, end = traversal.neighbor_source_boxes_starts[itgt_box:itgt_box+2]
+            for src_ibox in traversal.neighbor_source_boxes_lists[start:end]:
+                nlist1_srcs += tree.box_source_counts_nonchild[src_ibox]
+
+            nlist1_srcs_by_itgt_box[itgt_box] = nlist1_srcs
+
+            nlist3close_srcs = 0
+            # Could be None, if not using targets with extent.
+            if traversal.from_sep_close_smaller_starts is not None:
+                start, end = (
+                        traversal.from_sep_close_smaller_starts[itgt_box:itgt_box+2])
+                for src_ibox in traversal.from_sep_close_smaller_lists[start:end]:
+                    nlist3close_srcs += tree.box_source_counts_nonchild[src_ibox]
+
+            nlist3close_srcs_by_itgt_box[itgt_box] = nlist3close_srcs
+
+            nlist4close_srcs = 0
+            # Could be None, if not using targets with extent.
+            if traversal.from_sep_close_bigger_starts is not None:
+                start, end = (
+                        traversal.from_sep_close_bigger_starts[itgt_box:itgt_box+2])
+                for src_ibox in traversal.from_sep_close_bigger_lists[start:end]:
+                    nlist4close_srcs += tree.box_source_counts_nonchild[src_ibox]
+
+            nlist4close_srcs_by_itgt_box[itgt_box] = nlist4close_srcs
+
+        result = dict()
+        result["nlist1_srcs_by_itgt_box"] = nlist1_srcs_by_itgt_box
+        result["nlist3close_srcs_by_itgt_box"] = nlist3close_srcs_by_itgt_box
+        result["nlist4close_srcs_by_itgt_box"] = nlist4close_srcs_by_itgt_box
+
+        return result
---
+++ test/test_performance_model.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pyopencl as cl
+import time
+
+import pytest
+from pyopencl.tools import (  # noqa
+    pytest_generate_tests_for_pyopencl as pytest_generate_tests)
+
+
+@pytest.mark.opencl
+@pytest.mark.parametrize(
+    ("nsources", "ntargets", "dims", "dtype"), [
+        (5000, 5000, 3, np.float64)
+    ]
+)
+def test_performance_counter(ctx_factory, nsources, ntargets, dims, dtype):
+    ctx = ctx_factory()
+    queue = cl.CommandQueue(ctx)
+
+    # {{{ Generate sources, targets and target_radii
+
+    from boxtree.tools import make_normal_particle_array as p_normal
+    sources = p_normal(queue, nsources, dims, dtype, seed=15)
+    targets = p_normal(queue, ntargets, dims, dtype, seed=18)
+
+    from pyopencl.clrandom import PhiloxGenerator
+    rng = PhiloxGenerator(queue.context, seed=22)
+    target_radii = rng.uniform(
+        queue, ntargets, a=0, b=0.05, dtype=dtype
+    ).get()
+
+    # }}}
+
+    # {{{ Generate tree and traversal
+
+    from boxtree import TreeBuilder
+    tb = TreeBuilder(ctx)
+    tree, _ = tb(
+        queue, sources, targets=targets, target_radii=target_radii,
+        stick_out_factor=0.15, max_particles_in_box=60, debug=True
+    )
+
+    from boxtree.traversal import FMMTraversalBuilder
+    tg = FMMTraversalBuilder(ctx, well_sep_is_n_away=2)
+    d_trav, _ = tg(queue, tree, debug=True)
+    trav = d_trav.get(queue=queue)
+
+    # }}}
+
+    from boxtree.cost import CLCostCounter, PythonCostCounter
+    cl_cost_counter = CLCostCounter(queue)
+    python_cost_counter = PythonCostCounter()
+
+    start_time = time.time()
+    cl_direct_interaction = cl_cost_counter.collect_direct_interaction_data(
+        trav, trav.tree
+    )
+    print("OpenCL time {0}".format(str(time.time() - start_time)))
+    start_time = time.time()
+    python_direct_interaction = python_cost_counter.collect_direct_interaction_data(
+        trav, trav.tree
+    )
+    print("Python time {0}".format(str(time.time() - start_time)))
+
+    for field in ["nlist1_srcs_by_itgt_box"]:
+        assert np.equal(
+            cl_direct_interaction[field],
+            python_direct_interaction[field]
+        ).all()
+
+
+def main():
+    nsouces = 5000
+    ntargets = 5000
+    ndims = 3
+    dtype = np.float64
+    ctx_factory = cl.create_some_context
+
+    test_performance_counter(ctx_factory, nsouces, ntargets, ndims, dtype)
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        exec(sys.argv[1])
+    else:
+        main()
