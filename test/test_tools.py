9e6d1abca1850020db415a30f965327a24c475c0
Matt Wala <wala1@illinois.edu>
2019-05-29 23:35:15 -0500

    Fix test

--- test/test_tools.py
+++ test/test_tools.py
@@ -72,7 +72,7 @@ def test_list_renumberer(ctx_factory):
     arr = cl.array.to_device(queue, np.array([9, 4, 1, 4, 5, 6, 9]))
     lr = ListRenumberer(ctx, np.int)
 
-    (renumbered_arr, new_to_old), _ = lr(arr)
+    renumbered_arr, new_to_old, _ = lr(arr)
 
     assert (renumbered_arr.get() == np.array([4, 1, 0, 1, 2, 3, 4])).all()
     assert (new_to_old.get() == np.array([1, 4, 5, 6, 9])).all()
