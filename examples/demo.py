e279ce8dc4478758f53b361f9411a40f2ebd4ace
Andreas Kloeckner <inform@tiker.net>
2020-09-17 19:56:00 -0500

    More flake8-quotes placatin'

--- examples/demo.py
+++ examples/demo.py
@@ -50,15 +50,15 @@ plotter.set_bounding_box()
 pt.gca().set_aspect("equal")
 pt.tight_layout()
 pt.tick_params(
-    axis='x',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom='off',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    labelbottom='off')
+    axis="x",          # changes apply to the x-axis
+    which="both",      # both major and minor ticks are affected
+    bottom="off",      # ticks along the bottom edge are off
+    top="off",         # ticks along the top edge are off
+    labelbottom="off")
 pt.tick_params(
-    axis='y',
-    which='both',
-    left='off',
-    top='off',
-    labelleft='off')
+    axis="y",
+    which="both",
+    left="off",
+    top="off",
+    labelleft="off")
 pt.savefig("tree.pdf")
