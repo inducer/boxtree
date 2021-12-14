bfc06e411679f512225a62e5fc74fb2489627016
Andreas Klöckner <inform@tiker.net>
2021-12-14 11:37:04 -0600

    Separate kernel-dependent/tree-independent state out of wrangler (#29)
    * Register pytest markers
    
    * Expansion wranglers: do not store trees
    
    * Remove (unrealized) Wrangler.tree_dependent_info doc
    
    * Add sumpy downstream CI
    
    * Fix test failures for tree/wrangler refactor
    
    * Create fmm.py
    
    * More justification for TraversalAndWrangler design
    
    * Placate flake8
    
    * Back out ill-fated TraversalAndWrangler, introduce TreeIndependentDataForWrangler, introduce boxtree.{timing,constant_one}
    
    * Fix pylint/flake8 for tree-indep data for wrangler
    
    * Update the may-hold-tree comment in the wrangler docstring
    
    * Fix incorrect merge of drive_fmm docstring
    
    * Remove *zeros methods from wrangler interface
    
    * Tweak sumpy downstream to use appropriate branch
    
    * Fix downstream Github CI script
    
    * Fix downstream CI script syntax
    
    * Refactor so that FMMLibTreeIndependentDataForWrangler knows kernel but not Helmholtz k
    
    * Adjust downstream pytential CI for wrangler-refactor branch
    
    * Add template_ary arg to finalize_potentials
    
    * Add {multipole,local}_expansions_view to wrangler interface

--- boxtree/fmm.py
+++ boxtree/fmm.py
@@ -1,14 +1,8 @@
 """
 .. autofunction:: drive_fmm
 
+.. autoclass:: TreeIndependentDataForWrangler
 .. autoclass:: ExpansionWranglerInterface
-    :members:
-    :undoc-members:
-    :member-order: bysource
-
-.. autoclass:: TimingResult
-
-.. autoclass:: TimingFuture
 """
 
 __copyright__ = "Copyright (C) 2012 Andreas Kloeckner"
@@ -33,22 +27,250 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+from abc import ABC, abstractmethod
 import logging
 logger = logging.getLogger(__name__)
-
-
-try:
-    # Python 3
-    from collections.abc import Mapping
-except ImportError:
-    # Python 2
-    from collections import Mapping
+from boxtree.tree import Tree
+from boxtree.traversal import FMMTraversalInfo
 
 
 from pytools import ProcessLogger
 
 
-def drive_fmm(traversal, expansion_wrangler, src_weight_vecs, timing_data=None):
+# {{{ expansion wrangler interface
+
+class TreeIndependentDataForWrangler:
+    """An object that can be used to store information for efficient
+    wrangler execution that depends on the kernel but not the tree and/or
+    the traversal.
+
+    Examples of such data include generated code for carrying out
+    translations.
+
+    .. note::
+
+        Instances of this type should not hold a reference (and thereby be
+        specific to) a :class:`boxtree.Tree` instance. Their purpose is to
+        host caches for generated translation code that is reusable across
+        trees. It is OK for these instances to be specific to a given kernel
+        (or set of kernels).
+    """
+
+
+class ExpansionWranglerInterface(ABC):
+    """Abstract expansion handling interface for use with :func:`drive_fmm`.
+
+    See this
+    `test code <https://github.com/inducer/boxtree/blob/master/test/test_fmm.py>`_
+    for a very simple sample implementation.
+
+    .. note::
+
+        Wranglers may hold a reference (and thereby be specific to) a
+        :class:`boxtree.Tree` instance.
+        :class:`TreeIndependentDataForWrangler` exists to hold data that
+        is more broadly reusable.
+
+    Functions that support returning timing data return a value supporting the
+    :class:`~boxtree.timing.TimingFuture` interface.
+
+    .. versionchanged:: 2018.1
+
+        Changed (a subset of) functions to return timing data.
+
+    .. attribute:: tree_indep
+
+        An instance of (a typically wrangler-dependent subclass of)
+        :class:`TreeIndependentDataForWrangler`.
+
+    .. attribute:: traversal
+
+        An instance of :class:`~boxtree.traversal.FMMTraversalInfo`.
+
+    .. autoattribute:: tree
+
+    .. rubric:: Particle ordering
+
+    .. automethod:: reorder_sources
+    .. automethod:: reorder_potentials
+
+    .. rubric:: Views into arrays of expansions
+
+    .. automethod:: multipole_expansions_view
+    .. automethod:: local_expansions_view
+
+    .. rubric:: Translations
+
+    .. automethod:: form_multipoles
+    .. automethod:: coarsen_multipoles
+    .. automethod:: eval_direct
+    .. automethod:: multipole_to_local
+    .. automethod:: eval_multipoles
+    .. automethod:: form_locals
+    .. automethod:: refine_locals
+    .. automethod:: eval_locals
+    .. automethod:: finalize_potentials
+    """
+
+    def __init__(self, tree_indep: TreeIndependentDataForWrangler,
+            traversal: FMMTraversalInfo):
+        self.tree_indep = tree_indep
+        self.traversal = traversal
+
+    @property
+    def tree(self) -> Tree:
+        return self.traversal.tree
+
+    @abstractmethod
+    def reorder_sources(self, source_array):
+        """Return a copy of *source_array* in
+        :ref:`tree source order <particle-orderings>`.
+        *source_array* is in user source order.
+        """
+
+    @abstractmethod
+    def reorder_potentials(self, potentials):
+        """Return a copy of *potentials* in
+        :ref:`user target order <particle-orderings>`.
+        *source_weights* is in tree target order.
+        """
+
+    # {{{ views into arrays of expansions
+
+    # Included here for the benefit of the distributed-memory FMM
+
+    @abstractmethod
+    def multipole_expansions_view(self, mpole_exps, level):
+        pass
+
+    @abstractmethod
+    def local_expansions_view(self, local_exps, level):
+        pass
+
+    # }}}
+
+    # {{{ translations
+
+    @abstractmethod
+    def form_multipoles(self,
+            level_start_source_box_nrs, source_boxes,
+            src_weight_vecs):
+        """Return an expansions array
+        containing multipole expansions in *source_boxes* due to sources
+        with *src_weight_vecs*.
+        All other expansions must be zero.
+
+        :return: A pair (*mpoles*, *timing_future*).
+        """
+
+    @abstractmethod
+    def coarsen_multipoles(self,
+            level_start_source_parent_box_nrs,
+            source_parent_boxes, mpoles):
+        """For each box in *source_parent_boxes*,
+        gather (and translate) the box's children's multipole expansions in
+        *mpole* and add the resulting expansion into the box's multipole
+        expansion in *mpole*.
+
+        :returns: A pair (*mpoles*, *timing_future*).
+        """
+
+    @abstractmethod
+    def eval_direct(self,
+            target_boxes, neighbor_sources_starts,
+            neighbor_sources_lists, src_weight_vecs):
+        """For each box in *target_boxes*, evaluate the influence of the
+        neighbor sources due to *src_weight_vecs*, which use :ref:`csr` and are
+        indexed like *target_boxes*.
+
+        :returns: A pair (*pot*, *timing_future*), where *pot* is a
+            a new potential array.
+        """
+
+    @abstractmethod
+    def multipole_to_local(self,
+            level_start_target_or_target_parent_box_nrs,
+            target_or_target_parent_boxes,
+            starts, lists, mpole_exps):
+        """For each box in *target_or_target_parent_boxes*, translate and add
+        the influence of the multipole expansion in *mpole_exps* into a new
+        array of local expansions.  *starts* and *lists* use :ref:`csr`, and
+        *starts* is indexed like *target_or_target_parent_boxes*.
+
+        :returns: A pair (*pot*, *timing_future*) where *pot* is
+            a new (local) expansion array.
+        """
+
+    @abstractmethod
+    def eval_multipoles(self,
+            target_boxes_by_source_level, from_sep_smaller_by_level, mpole_exps):
+        """For a level *i*, each box in *target_boxes_by_source_level[i]*, evaluate
+        the multipole expansion in *mpole_exps* in the nearby boxes given in
+        *from_sep_smaller_by_level*, and return a new potential array.
+        *starts* and *lists* in *from_sep_smaller_by_level[i]* use :ref:`csr`
+        and *starts* is indexed like *target_boxes_by_source_level[i]*.
+
+        :returns: A pair (*pot*, *timing_future*) where *pot* is a new potential
+            array.
+        """
+
+    @abstractmethod
+    def form_locals(self,
+            level_start_target_or_target_parent_box_nrs,
+            target_or_target_parent_boxes, starts, lists, src_weight_vecs):
+        """For each box in *target_or_target_parent_boxes*, form local
+        expansions due to the sources in the nearby boxes given in *starts* and
+        *lists*, and return a new local expansion array.  *starts* and *lists*
+        use :ref:`csr` and *starts* is indexed like
+        *target_or_target_parent_boxes*.
+
+        :returns: A pair (*pot*, *timing_future*) where *pot* is a new
+            local expansion array.
+        """
+
+    @abstractmethod
+    def refine_locals(self,
+            level_start_target_or_target_parent_box_nrs,
+            target_or_target_parent_boxes, local_exps):
+        """For each box in *child_boxes*,
+        translate the box's parent's local expansion in *local_exps* and add
+        the resulting expansion into the box's local expansion in *local_exps*.
+
+        :returns: A pair (*local_exps*, *timing_future*).
+        """
+
+    @abstractmethod
+    def eval_locals(self,
+            level_start_target_box_nrs, target_boxes, local_exps):
+        """For each box in *target_boxes*, evaluate the local expansion in
+        *local_exps* and return a new potential array.
+
+        :returns: A pair (*pot*, *timing_future*) where *pot* is a new potential
+            array.
+        """
+
+    # }}}
+
+    @abstractmethod
+    def finalize_potentials(self, potentials, template_ary):
+        """
+        Postprocess the reordered potentials. This is where global scaling
+        factors could be applied. This is distinct from :meth:`reorder_potentials`
+        because some derived FMMs (notably the QBX FMM) do their own reordering.
+
+        :arg template_ary: If the array type used inside of the FMM
+            is different from the array type used by the user (e.g.
+            :class:`boxtree.pyfmmlib_integration.FMMLibExpansionWrangler`
+            uses :class:`numpy.ndarray` internally, this array can be used
+            to help convert the output back to the user's array
+            type (typically :class:`pyopencl.array.Array`).
+        """
+
+# }}}
+
+
+def drive_fmm(wrangler: ExpansionWranglerInterface, src_weight_vecs,
+        timing_data=None):
     """Top-level driver routine for a fast multipole calculation.
 
     In part, this is intended as a template for custom FMMs, in the sense that
@@ -59,24 +281,24 @@ def drive_fmm(traversal, expansion_wrangler, src_weight_vecs, timing_data=None):
     Nonetheless, many common applications (such as point-to-point FMMs) can be
     covered by supplying the right *expansion_wrangler* to this routine.
 
-    :arg traversal: A :class:`boxtree.traversal.FMMTraversalInfo` instance.
     :arg expansion_wrangler: An object exhibiting the
         :class:`ExpansionWranglerInterface`.
     :arg src_weight_vecs: A sequence of source 'density/weights/charges'.
         Passed unmodified to *expansion_wrangler*.
     :arg timing_data: Either *None*, or a :class:`dict` that is populated with
         timing information for the stages of the algorithm (in the form of
-        :class:`TimingResult`), if such information is available.
+        :class:`~boxtree.timing.TimingResult`), if such information is available.
 
     Returns the potentials computed by *expansion_wrangler*.
-
     """
-    wrangler = expansion_wrangler
+
+    traversal = wrangler.traversal
 
     # Interface guidelines: Attributes of the tree are assumed to be known
     # to the expansion wrangler and should not be passed.
 
     fmm_proc = ProcessLogger(logger, "fmm")
+    from boxtree.timing import TimingRecorder
     recorder = TimingRecorder()
 
     src_weight_vecs = [wrangler.reorder_sources(weight) for
@@ -219,7 +441,7 @@ def drive_fmm(traversal, expansion_wrangler, src_weight_vecs, timing_data=None):
 
     result = wrangler.reorder_potentials(potentials)
 
-    result = wrangler.finalize_potentials(result)
+    result = wrangler.finalize_potentials(result, template_ary=src_weight_vecs[0])
 
     fmm_proc.done()
 
@@ -227,245 +449,6 @@ def drive_fmm(traversal, expansion_wrangler, src_weight_vecs, timing_data=None):
         timing_data.update(recorder.summarize())
 
     return result
-
-
-# {{{ expansion wrangler interface
-
-class ExpansionWranglerInterface:
-    """Abstract expansion handling interface for use with :func:`drive_fmm`.
-
-    See this
-    `test code <https://github.com/inducer/boxtree/blob/master/test/test_fmm.py>`_
-    for a very simple sample implementation.
-
-    Will usually hold a reference (and thereby be specific to) a
-    :class:`boxtree.Tree` instance.
-
-    Functions that support returning timing data return a value supporting the
-    :class:`TimingFuture` interface.
-
-    .. versionchanged:: 2018.1
-
-        Changed (a subset of) functions to return timing data.
-    """
-
-    def multipole_expansion_zeros(self):
-        """Return an expansions array (which must support addition)
-        capable of holding one multipole or local expansion for every
-        box in the tree.
-        """
-
-    def local_expansion_zeros(self):
-        """Return an expansions array (which must support addition)
-        capable of holding one multipole or local expansion for every
-        box in the tree.
-        """
-
-    def output_zeros(self):
-        """Return a potentials array (which must support addition) capable of
-        holding a potential value for each target in the tree. Note that
-        :func:`drive_fmm` makes no assumptions about *potential* other than
-        that it supports addition--it may consist of potentials, gradients of
-        the potential, or arbitrary other per-target output data.
-        """
-
-    def reorder_sources(self, source_array):
-        """Return a copy of *source_array* in
-        :ref:`tree source order <particle-orderings>`.
-        *source_array* is in user source order.
-        """
-
-    def reorder_potentials(self, potentials):
-        """Return a copy of *potentials* in
-        :ref:`user target order <particle-orderings>`.
-        *source_weights* is in tree target order.
-        """
-
-    def form_multipoles(self, level_start_source_box_nrs, source_boxes,
-            src_weight_vecs):
-        """Return an expansions array (compatible with
-        :meth:`multipole_expansion_zeros`)
-        containing multipole expansions in *source_boxes* due to sources
-        with *src_weight_vecs*.
-        All other expansions must be zero.
-
-        :return: A pair (*mpoles*, *timing_future*).
-        """
-
-    def coarsen_multipoles(self, level_start_source_parent_box_nrs,
-            source_parent_boxes, mpoles):
-        """For each box in *source_parent_boxes*,
-        gather (and translate) the box's children's multipole expansions in
-        *mpole* and add the resulting expansion into the box's multipole
-        expansion in *mpole*.
-
-        :returns: A pair (*mpoles*, *timing_future*).
-        """
-
-    def eval_direct(self, target_boxes, neighbor_sources_starts,
-            neighbor_sources_lists, src_weight_vecs):
-        """For each box in *target_boxes*, evaluate the influence of the
-        neighbor sources due to *src_weight_vecs*, which use :ref:`csr` and are
-        indexed like *target_boxes*.
-
-        :returns: A pair (*pot*, *timing_future*), where *pot* is a
-            a new potential array, see :meth:`output_zeros`.
-        """
-
-    def multipole_to_local(self,
-            level_start_target_or_target_parent_box_nrs,
-            target_or_target_parent_boxes,
-            starts, lists, mpole_exps):
-        """For each box in *target_or_target_parent_boxes*, translate and add
-        the influence of the multipole expansion in *mpole_exps* into a new
-        array of local expansions.  *starts* and *lists* use :ref:`csr`, and
-        *starts* is indexed like *target_or_target_parent_boxes*.
-
-        :returns: A pair (*pot*, *timing_future*) where *pot* is
-            a new (local) expansion array, see :meth:`local_expansion_zeros`.
-        """
-
-    def eval_multipoles(self,
-            target_boxes_by_source_level, from_sep_smaller_by_level, mpole_exps):
-        """For a level *i*, each box in *target_boxes_by_source_level[i]*, evaluate
-        the multipole expansion in *mpole_exps* in the nearby boxes given in
-        *from_sep_smaller_by_level*, and return a new potential array.
-        *starts* and *lists* in *from_sep_smaller_by_level[i]* use :ref:`csr`
-        and *starts* is indexed like *target_boxes_by_source_level[i]*.
-
-        :returns: A pair (*pot*, *timing_future*) where *pot* is a new potential
-            array, see :meth:`output_zeros`.
-        """
-
-    def form_locals(self,
-            level_start_target_or_target_parent_box_nrs,
-            target_or_target_parent_boxes, starts, lists, src_weight_vecs):
-        """For each box in *target_or_target_parent_boxes*, form local
-        expansions due to the sources in the nearby boxes given in *starts* and
-        *lists*, and return a new local expansion array.  *starts* and *lists*
-        use :ref:`csr` and *starts* is indexed like
-        *target_or_target_parent_boxes*.
-
-        :returns: A pair (*pot*, *timing_future*) where *pot* is a new
-            local expansion array, see :meth:`local_expansion_zeros`.
-        """
-
-    def refine_locals(self, level_start_target_or_target_parent_box_nrs,
-            target_or_target_parent_boxes, local_exps):
-        """For each box in *child_boxes*,
-        translate the box's parent's local expansion in *local_exps* and add
-        the resulting expansion into the box's local expansion in *local_exps*.
-
-        :returns: A pair (*local_exps*, *timing_future*).
-        """
-
-    def eval_locals(self, level_start_target_box_nrs, target_boxes, local_exps):
-        """For each box in *target_boxes*, evaluate the local expansion in
-        *local_exps* and return a new potential array.
-
-        :returns: A pair (*pot*, *timing_future*) where *pot* is a new potential
-            array, see :meth:`output_zeros`.
-        """
-
-    def finalize_potentials(self, potentials):
-        """
-        Postprocess the reordered potentials. This is where global scaling
-        factors could be applied. This is distinct from :meth:`reorder_potentials`
-        because some derived FMMs (notably the QBX FMM) do their own reordering.
-        """
-
-# }}}
-
-
-# {{{ timing result
-
-class TimingResult(Mapping):
-    """Interface for returned timing data.
-
-    This supports accessing timing results via a mapping interface, along with
-    combining results via :meth:`merge`.
-
-    .. automethod:: __init__
-    .. automethod:: merge
-    """
-
-    def __init__(self, *args, **kwargs):
-        """See constructor for :class:`dict`."""
-        self._mapping = dict(*args, **kwargs)
-
-    def __getitem__(self, key):
-        return self._mapping[key]
-
-    def __iter__(self):
-        return iter(self._mapping)
-
-    def __len__(self):
-        return len(self._mapping)
-
-    def merge(self, other):
-        """Merge this result with another by adding together common fields."""
-        result = {}
-
-        for key in self:
-            val = self.get(key)
-            other_val = other.get(key)
-
-            if val is None or other_val is None:
-                continue
-
-            result[key] = val + other_val
-
-        return type(self)(result)
-
-# }}}
-
-
-# {{{ timing future
-
-class TimingFuture:
-    """Returns timing data for a potentially asynchronous operation.
-
-    .. automethod:: result
-    .. automethod:: done
-    """
-
-    def result(self):
-        """Return a :class:`TimingResult`. May block."""
-        raise NotImplementedError
-
-    def done(self):
-        """Return *True* if the operation is complete."""
-        raise NotImplementedError
-
-# }}}
-
-
-# {{{ timing recorder
-
-class TimingRecorder:
-
-    def __init__(self):
-        from collections import defaultdict
-        self.futures = defaultdict(list)
-
-    def add(self, description, future):
-        self.futures[description].append(future)
-
-    def summarize(self):
-        result = {}
-
-        for description, futures_list in self.futures.items():
-            futures = iter(futures_list)
-
-            timing_result = next(futures).result()
-            for future in futures:
-                timing_result = timing_result.merge(future.result())
-
-            result[description] = timing_result
-
-        return result
-
-# }}}
 
 
 # vim: filetype=pyopencl:fdm=marker
