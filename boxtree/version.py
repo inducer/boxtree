b571e9b5e6d2c15e814ea6ddc6af161609c74f26
Alexandru Fikl <alexfikl@gmail.com>
2024-07-21 16:51:11 +0300

    version: use importlib

--- boxtree/version.py
+++ boxtree/version.py
@@ -1,2 +1,15 @@
-VERSION = (2023, 1)
-VERSION_TEXT = ".".join(str(i) for i in VERSION)
+from importlib import metadata
+from typing import Tuple
+
+
+def _parse_version(version: str) -> Tuple[Tuple[int, ...], str]:
+    import re
+
+    m = re.match("^([0-9.]+)([a-z0-9]*?)$", VERSION_TEXT)
+    assert m is not None
+
+    return tuple(int(nr) for nr in m.group(1).split(".")), m.group(2)
+
+
+VERSION_TEXT = metadata.version("boxtree")
+VERSION, VERSION_STATUS = _parse_version(VERSION_TEXT)
